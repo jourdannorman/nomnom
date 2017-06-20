65d3300ce52049b8ff274ec7ff97582332e552a2
Jourdan Norman <jourdan.norman@icloud.com>
2017-06-20 21:27:57 +0000

    Created Place detail page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
 
 	def index
 		# @places = Place.all
-		@places = Place.paginate(:page => params[:page], :per_page => 5)
+		@places = Place.paginate(:page => params[:page], :per_page => 3)
 	end
 
 	def new
@@ -13,6 +13,10 @@ class PlacesController < ApplicationController
 	def create
 		current_user.places.create(place_params)
 		redirect_to root_path
+	end
+
+	def show
+		@place = Place.find(params[:id])
 	end
 
 	private
